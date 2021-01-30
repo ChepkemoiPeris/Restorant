a299ec96e806bbda42988cc2ff87c7bbf06ca30e
perismemo <pmemoi2015@gmail.com>
2021-01-30 23:40:37 +0300

    first commit

---
+++ admin/manage-admin.php
@@ -0,0 +1,19 @@
+<!Doctype html>
+<html>
+<head>
+    <title>Food order website</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+<?php include('partials/menu.php'); ?>
+
+<!-- main content section starts here -->
+<div class="main-content">
+    <div class="wrapper">
+        <strong>Manage Admin</strong>
+
+    </div>
+</div>
+<!-- main content section ends here -->
+
+<?php include('partials/footer.php');?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<!-- footer section starts here -->
+<div class="footer">
+    <div class="wrapper">
+        <p class="text-center">2020 All rights reserved, Restaurant. Developed By-<a href="#">Peris</a> </p>
+    </div>
+</div>
+<!-- footer section ends here -->
+</body>
+</html>
